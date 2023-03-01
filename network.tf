e11db501dc26c408aa11d9872fc3127950b6d43b
Grace L <grace.li@gov.ab.ca>
2023-03-01 22:41:31 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -2,6 +2,6 @@ module "network" {
   source  = "app.terraform.io/gracel-training/network/azurerm"
   version = "3.5.0"
   # insert required variables 
-resource_group_name = "azurerm_resource_group"
+  resource_group_name = "azurerm_resource_group"
 }
 
